4f5407eca8094e2f3be98dfd838d7ff8b72a5388
aospiridonov <aospiridonov@gmail.com>
2021-02-03 16:55:31 +0300

    add post request to add order

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import './cart.dart';
 
 class OrderItem {
@@ -22,14 +24,33 @@ class Orders with ChangeNotifier {
     return _orders;
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url =
+        'https://flutter-update-73816-default-rtdb.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    final response = await http.post(url,
+        body: json.encode(
+          {
+            'amount': total,
+            'dateTime': timestamp.toIso8601String(),
+            'products': cartProducts
+                .map((cartProduct) => {
+                      'id': cartProduct.id,
+                      'title': cartProduct.title,
+                      'quantity': cartProduct.quantity,
+                      'price': cartProduct.price,
+                    })
+                .toList(),
+          },
+        ));
+
     _orders.insert(
         0,
         OrderItem(
-            id: DateTime.now().toString(),
+            id: json.decode(response.body)['name'],
             amount: total,
             products: cartProducts,
-            dateTime: DateTime.now()));
+            dateTime: timestamp));
     notifyListeners();
   }
 }
